b750f772cbaf9b167b203f7357bb4701db863c32
CasheeeewNuts <casheeeewnuts+tech@gmail.com>
2021-07-08 00:32:08 +0900

    implement bundling main process

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,28 @@
 import * as fs from "fs";
+import path from "path";
 
-export default function (path: string) {
-  return fs.readFileSync(path).toString()
+
+const TAG_REGEX = /^!\[(.+)]/;
+
+export default function (filePath: string) {
+  let buffer: Buffer = Buffer.alloc(0);
+
+  const rootFile = fs.readFileSync(filePath);
+  const rootFileDirectory = path.dirname(filePath);
+
+  rootFile.toString().split('\n').forEach(line => {
+    if (!line.match(TAG_REGEX)) {
+      buffer.write(line);
+      console.log(line)
+    }
+
+    const includeFilePath = line.replace(TAG_REGEX, '$1');
+    const resolvedIncludeFilePath = path.isAbsolute(includeFilePath)
+        ? includeFilePath
+        : path.resolve(rootFileDirectory, includeFilePath);
+
+    buffer = Buffer.concat([buffer, fs.readFileSync(resolvedIncludeFilePath), Buffer.from('\n')]);
+  })
+
+  return buffer.toString();
 }
